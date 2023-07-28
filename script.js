2a26998aa38bcd286e064cc605c45326f6bb4dc2
mannmanjeet988 <mannmanjeet988@gmail.com>
2023-07-29 01:38:30 +0530

    Adding sheets Feature

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const thead = document.getElementById("table-heading-row");
-const tbody =document.getElementById("table-body");
+const tbody = document.getElementById("table-body");
 
 const boldBtn = document.getElementById("bold-btn");
 const italicsBtn = document.getElementById("italics-btn");
@@ -19,238 +19,318 @@ const leftAlignBtn = document.getElementById("left-align-btn");
 const centerAlignBtn = document.getElementById("center-align--btn");
 const rightAlignBtn = document.getElementById("right-align--btn");
 
-const uploadJsonFile = document.getElementById('jsonFile');
+const uploadJsonFile = document.getElementById("jsonFile");
+const addSheetButton = document.getElementById("add-sheet-button");
+const buttonContainer = document.getElementById("button-container");
+const sheetNo = document.getElementById("sheet-no");
 
-const cols=26;
-const rows=100;
+const cols = 26;
+const rows = 100;
 let currCell;
-cutCell={};
+let currentsheetid = 1;
+
+cutCell = {};
+
+let matrixtemplate = [];
+for (let i = 0; i < rows; i++) {
+  let temprow = [];
+  for (let j = 0; j < cols; j++) {
+    temprow.push({});
+  }
+  matrixtemplate.push(temprow);
+}
+localStorage.setItem("arrMatrix", JSON.stringify([matrixtemplate]));
 
 
 let matrix = new Array(rows);
-function createMatrix(){
-    for(let i=0;i<rows;i++){
-        matrix[i]=new Array(cols);
-        for(let j=0;j<cols;j++){
-            matrix[i][j]={};
-        }
-    }   
+function createMatrix() {
+  for (let i = 0; i < rows; i++) {
+    matrix[i] = new Array(cols);
+    for (let j = 0; j < cols; j++) {
+      matrix[i][j] = {};
+    }
+  }
 }
 
+for (let col = 0; col < cols; col++) {
+  let th = document.createElement("th");
+  th.innerText = String.fromCharCode(65 + col);
+  thead.appendChild(th);
+}
 
-function createTable(){
+function createTable() {
+  for (let row = 1; row <= rows; row++) {
+    let tr = document.createElement("tr");
+    tr.innerText = row;
+    tbody.appendChild(tr);
     for (let col = 0; col < cols; col++) {
-        let th = document.createElement("th");
-        th.innerText = String.fromCharCode(65 + col);
-        thead.appendChild(th);
-    }
-      
-    for(let row =1; row<=rows; row++){
-        let tr = document.createElement("tr");
-        tr.innerText= row;
-        tbody.appendChild(tr);
-        for(let col=0; col< cols; col++){
-            let td = document.createElement("td");
-            td.setAttribute("contenteditable","true");
-            td.setAttribute("spellCheck","false");
-             // this event will revolve around input
-             td.addEventListener('input', (event) => onInputFun(event));
+      let td = document.createElement("td");
+      td.setAttribute("contenteditable", "true");
+      td.setAttribute("spellCheck", "false");
+      // this event will revolve around input
+      td.addEventListener("input", (event) => onInputFun(event));
 
-             // this event revolves around focus on a cell
-            td.addEventListener("focus",(event)=>onFocusFun(event));
+      // this event revolves around focus on a cell
+      td.addEventListener("focus", (event) => onFocusFun(event));
 
-            td.setAttribute("id",`${String.fromCharCode(65 + col)}${row}`)
-            tr.appendChild(td); 
-        }
+      td.setAttribute("id", `${String.fromCharCode(65 + col)}${row}`);
+      tr.appendChild(td);
     }
+  }
 }
 
 createTable();
 createMatrix();
 
-function updateMatrix(currCell){
-    let tempObj={
-        style: currCell.style.cssText,
-        text: currCell.innerText,
-        id: currCell.id,
-    }
-    let i = currCell.id.substring(1)-1;
-    let j = currCell.id[0].charCodeAt(0)-65;
-     // currentCell.id[0] -> this will give me character
-    // str.chatCodeAt(i) will give me respective ascii at ith index of string str
-    // -65 for making ascii code to 0th index
-    matrix[i][j]=tempObj;
+function updateMatrix(currCell) {
+  let tempObj = {
+    style: currCell.style.cssText,
+    text: currCell.innerText,
+    id: currCell.id,
+  };
+  let i = currCell.id.substring(1) - 1;
+  let j = currCell.id[0].charCodeAt(0) - 65;
+  // currentCell.id[0] -> this will give me character
+  // str.chatCodeAt(i) will give me respective ascii at ith index of string str
+  // -65 for making ascii code to 0th index
+  let matrixArrdata = JSON.parse(localStorage.getItem("arrMatrix"));
+  console.log("id", currentsheetid);
+  matrixArrdata[currentsheetid - 1][i][j] = tempObj;
+  localStorage.setItem("arrMatrix", JSON.stringify(matrixArrdata));
 }
 
-function onFocusFun(event){
-    console.log("In focus:", event.target);
-    currCell = event.target;
-    document.getElementById("current-cell").innerText = event.target.id;
-    console.log(currCell.style.cssText);
-    if (currCell.style.fontWeight === 'bold') {
-        boldBtn.style.backgroundColor = 'yellow';
-    }
-    else boldBtn.style.backgroundColor = 'transparent';
-//   console.log(currCell.innerText);
-//   console.log(currCell.id);
-    updateMatrix(currCell);
+function onFocusFun(event) {
+  console.log("In focus:", event.target);
+  currCell = event.target;
+  document.getElementById("current-cell").innerText = event.target.id;
+  console.log(currCell.style.cssText);
+  if (currCell.style.fontWeight === "bold") {
+    boldBtn.style.backgroundColor = "yellow";
+  } else boldBtn.style.backgroundColor = "transparent";
+  //   console.log(currCell.innerText);
+  //   console.log(currCell.id);
+  updateMatrix(currCell);
 }
 
-function onInputFun(event){
-    console.log("input:", event.target);
-    updateMatrix(event.target);
-      // id
-    // cell content -> innerText
-    // cell style -> cssText
+function onInputFun(event) {
+  console.log("input:", event.target);
+  updateMatrix(event.target);
+  // id
+  // cell content -> innerText
+  // cell style -> cssText
 }
- 
-boldBtn.addEventListener("click",()=>{
-    if(currCell.style.fontWeight == "bold"){
-        currCell.style.fontWeight = "normal";
-    }
-    else{
-        currCell.style.fontWeight = "bold" ;   
-    }
-    console.log("bold",currCell);
-    // latest style should be passed to updated matrix
-    updateMatrix(currCell);
-})
 
-italicsBtn.addEventListener("click",()=>{
-    if(currCell.style.fontStyle == "italic"){
-        currCell.style.fontStyle = "normal";
-    }
-    else{
-        currCell.style.fontStyle = "italic" ;   
-    }
-    console.log("italics",currCell);
-    // latest style should be passed to updated matrix
-    updateMatrix(currCell);
-})
+boldBtn.addEventListener("click", () => {
+  if (currCell.style.fontWeight == "bold") {
+    currCell.style.fontWeight = "normal";
+  } else {
+    currCell.style.fontWeight = "bold";
+  }
+  console.log("bold", currCell);
+  // latest style should be passed to updated matrix
+  updateMatrix(currCell);
+});
 
-underlineBtn.addEventListener("click",()=>{
-    if(currCell.style.textDecoration == "underline"){
-        currCell.style.textDecoration = null;
-    }
-    else{
-        currCell.style.textDecoration = "underline" ;   
-    }
-    console.log("underline",currCell);
-    // latest style should be passed to updated matrix
-    updateMatrix(currCell);
-})
+italicsBtn.addEventListener("click", () => {
+  if (currCell.style.fontStyle == "italic") {
+    currCell.style.fontStyle = "normal";
+  } else {
+    currCell.style.fontStyle = "italic";
+  }
+  console.log("italics", currCell);
+  // latest style should be passed to updated matrix
+  updateMatrix(currCell);
+});
 
-textColor.addEventListener("input",()=>{
-    currCell.style.color = textColor.value;
-    updateMatrix(currCell);
-})
+underlineBtn.addEventListener("click", () => {
+  if (currCell.style.textDecoration == "underline") {
+    currCell.style.textDecoration = null;
+  } else {
+    currCell.style.textDecoration = "underline";
+  }
+  console.log("underline", currCell);
+  // latest style should be passed to updated matrix
+  updateMatrix(currCell);
+});
 
-bgColor.addEventListener("input",()=>{
-    currCell.style.backgroundColor = bgColor.value;
-    updateMatrix(currCell)
-})
+textColor.addEventListener("input", () => {
+  currCell.style.color = textColor.value;
+  updateMatrix(currCell);
+});
 
-leftAlignBtn.addEventListener("click",()=>{
-    currCell.style.textAlign ="left";
-    updateMatrix(currCell);
-})
+bgColor.addEventListener("input", () => {
+  currCell.style.backgroundColor = bgColor.value;
+  updateMatrix(currCell);
+});
 
-centerAlignBtn.addEventListener("click",()=>{
-    currCell.style.textAlign ="center";
-    updateMatrix(currCell);
-})
+leftAlignBtn.addEventListener("click", () => {
+  currCell.style.textAlign = "left";
+  updateMatrix(currCell);
+});
 
-rightAlignBtn.addEventListener("click",()=>{
-    currCell.style.textAlign ="right";
-    updateMatrix(currCell);
-})
+centerAlignBtn.addEventListener("click", () => {
+  currCell.style.textAlign = "center";
+  updateMatrix(currCell);
+});
 
-// fontSizeDropDown.addEventListener("change",() => {
-//     currCell.style.fontSize = fontSizeDropDown.value;
-//     // what ever option tag is chosen by the end user is
-//     // mapped with select tag with value attribute
-//     updateMatrix(currCell);
-// })
+rightAlignBtn.addEventListener("click", () => {
+  currCell.style.textAlign = "right";
+  updateMatrix(currCell);
+});
 
+fontSizeDropDown.addEventListener("change", () => {
+  currCell.style.fontSize = fontSizeDropDown.value;
+  // what ever option tag is chosen by the end user is
+  // mapped with select tag with value attribute
+  updateMatrix(currCell);
+});
 
-// fontFamilyDropDown.addEventListener("change",()=>{
-//     currCell.style.fontFamily = fontFamilyDropDown.value;
-//     updateMatrix(currCell);
-// })
+fontFamilyDropDown.addEventListener("change", () => {
+  currCell.style.fontFamily = fontFamilyDropDown.value;
+  updateMatrix(currCell);
+});
 
+cutBtn.addEventListener("click", () => {
+  cutCell = {
+    text: currCell.innerText,
+    style: currCell.style.cssText,
+  };
+  currCell.innerText = "";
+  currCell.style.cssText = "";
+  updateMatrix(currCell);
+});
 
-cutBtn.addEventListener("click",()=>{
-     cutCell={
-        text: currCell.innerText,
-        style:currCell.style.cssText  
-     }
-     currCell.innerText='';
-       currCell.style.cssText =''; 
-       updateMatrix(currCell);
-})
+copyBtn.addEventListener("click", () => {
+  cutCell = {
+    text: currCell.innerText,
+    style: currCell.style.cssText,
+  };
+});
 
-copyBtn.addEventListener("click",()=>{
-    cutCell={
-       text: currCell.innerText,
-       style:currCell.style.cssText  
-    }
-})
-
-pasteBtn.addEventListener("click",()=>{
-    currCell.innerText= cutCell.text;
-    currCell.style.cssText=cutCell.style;
-    updateMatrix(currCell);
-})
+pasteBtn.addEventListener("click", () => {
+  currCell.innerText = cutCell.text;
+  currCell.style.cssText = cutCell.style;
+  updateMatrix(currCell);
+});
 
 function downloadJson() {
-    // 2d matrix into string
-    const matrixString = JSON.stringify(matrix);
-
-    // text form of matrix -> piece of memory (downloadable)
-    // application/json -> format for json
-    const blob = new Blob([matrixString], { type: 'application/json' });
-    // link created -> attach href
-    // click link
-    // delete link
-    const link = document.createElement('a');
-    // 211 -> converting piece of memory to downloadable link
-    link.href = URL.createObjectURL(blob);
-    // download the link instead of opening it
-    // link.download -> file name
-    link.download = 'table.json';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  // 2d matrix into string
+  const matrixString = JSON.stringify(matrix);
+  // text form of matrix -> piece of memory (downloadable)
+  // application/json -> format for json
+  const blob = new Blob([matrixString], { type: "application/json" });
+  // link created -> attach href
+  // click link
+  // delete link
+  const link = document.createElement("a");
+  // 211 -> converting piece of memory to downloadable link
+  link.href = URL.createObjectURL(blob);
+  // download the link instead of opening it
+  // link.download -> file name
+  link.download = "table.json";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
-uploadJsonFile.addEventListener('change', uploadJSONFileFn);
+uploadJsonFile.addEventListener("change", uploadJSONFileFn);
 
 function uploadJSONFileFn(event) {
-    const file = event.target.files[0];
-    if (file) {
-        // this can read external file
-        const reader = new FileReader();
-        // how you you trigger reader?
-        // .readAsText method will trigger reader and it will run onload default method    
-        reader.readAsText(file);
-        reader.onload = function (e) {
-            const fileContent = e.target.result;
-            try{
-                 // updated my matrix
-                 matrix = JSON.parse(fileContent);
-                 matrix.forEach(row=>{
-                      row.forEach((cell)=>{
-                        if(cell.id){
-                            let cellToBeEdited= document.getElementById(cell.id);
-                            cellToBeEdited.innerText = cell.text;
-                            cellToBeEdited.style.cssText = cell.style;
-                        }
-                        // else empty object-> do nothing
-                      })
-                 })
-            } catch(err){
-                console.log(err);
+  const file = event.target.files[0];
+  if (file) {
+    // this can read external file
+    const reader = new FileReader();
+    // how you you trigger reader?
+    // .readAsText method will trigger reader and it will run onload default method
+    reader.readAsText(file);
+    reader.onload = function (e) {
+      const fileContent = e.target.result;
+      try {
+        // updated my matrix
+        matrix = JSON.parse(fileContent);
+        matrix.forEach((row) => {
+          row.forEach((cell) => {
+            if (cell.id) {
+              let cellToBeEdited = document.getElementById(cell.id);
+              cellToBeEdited.innerText = cell.text;
+              cellToBeEdited.style.cssText = cell.style;
             }
+            // else empty object-> do nothing
+          });
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  }
+}
+//const arrMatrix = 'arrMatrix';
+
+//arrMatrix - [matrix1, matrix2, matrix3]
+let numSheets = 1;
+let currSheetNum = 1;
+
+addSheetButton.addEventListener("click", () => {
+  const btn = document.createElement("button");
+  numSheets++;
+  currSheetNum = numSheets;
+  btn.innerText = `Sheet ${numSheets}`;
+  btn.setAttribute("id", `sheet-${currSheetNum}`);
+  btn.setAttribute("onclick", "viewSheet(event)");
+  buttonContainer.append(btn);
+
+  // storing the previous matrix(table)
+  if (localStorage.getItem("arrMatrix")) {
+    var oldMatrixArr = localStorage.getItem("arrMatrix");
+
+    //oldMatrixArr ---> string
+    var newMatrixArr = [...JSON.parse(oldMatrixArr), matrixtemplate];
+    // console.log("old",oldMatrixArr);
+    // console.log(newMatrixArr);
+    // appending(pushing) matrix to oldMatrixArr
+    // var newMatrixArr = JSON.parse(oldMatrixArr)
+    // newMatrixArr.push(matrix)
+    localStorage.setItem("arrMatrix", JSON.stringify(newMatrixArr));
+  } else {
+    let tempMatrixArr = [matrix];
+    localStorage.setItem("arrMatrix", JSON.stringify(tempMatrixArr));
+  }
+
+  // clean up virtual memory
+  createMatrix();
+  sheetNo.innerText = `Sheet No - ${currSheetNum}`;
+  currentsheetid = currSheetNum;
+  tbody.innerHTML = ``;
+  createTable();
+});
+
+function viewSheet(event) {
+  console.log("viewsheet:", event.target);
+  let id = event.target.id.split("-")[1];
+  currentsheetid = id;
+  console.log(currentsheetid);
+  var matrixArr = JSON.parse(localStorage.getItem("arrMatrix"));
+  matrix = matrixArr[id - 1];
+  //console.log(matrix[0][0]);
+  // current matrix points towards the latest currentsheet
+  // clean previous table;
+  tbody.innerHTML = ``;
+  createTable();
+
+  matrix.forEach((row) => {
+    row.forEach((cell) => {
+      try {
+        if (cell.id) {
+          let cellToBeEdited = document.getElementById(cell.id);
+          cellToBeEdited.innerText = cell.text;
+          cellToBeEdited.style.cssText = cell.style;
         }
-    }
+        // else empty object-> do nothing
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
+  currSheetNum = id;
+  sheetNo.innerText = `Sheet No - ${currSheetNum}`;
 }
